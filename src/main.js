73cd722019c72a8717ad8055ab8ceeca68d566ca
Xinlai He <Xinlai_He@epam.com>
2016-02-25 15:21:08 +0800

    New Animation

--- src/main.js
+++ src/main.js
@@ -3,4 +3,18 @@ $(function(){
     $("button").click(function(){
        window.location.href="main.html";
     });
+
+    $(".menu button").mouseover(function(){
+        $(".menu input").animate({
+            width : "50%"
+        }, 500, "linear");
+
+    });
+    $(".menu button").mouseout(function(){
+        $(".menu input").animate({
+            width : "90%"
+        }, 500, "linear");
+
+    });
+
 });
